93780ef21417ece046af2a3eed65a01cdc87d847
Steven Atkinson <steven.atkinson@gmx.com>
2016-05-19 11:16:48 +0100

    Added the serializer and contract resolver.

---
+++ JsonSerializer.SnakeCase/SnakeCaseContractResolver.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace JsonSerializer.SnakeCase
+{
+    public class SnakeCaseContractResolver : DefaultContractResolver
+    {
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            return GetSnakeCase(propertyName);
+        }
+
+        private static string GetSnakeCase(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            var buffer = "";
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var isLast = (i == input.Length - 1);
+                var isSecondFromLast = (i == input.Length - 2);
+
+                var current = input[i];
+                var next = !isLast ? input[i + 1] : '\0';
+                var afterNext = !isSecondFromLast && !isLast ? input[i + 2] : '\0';
+
+                buffer += char.ToLower(current);
+
+                if (!char.IsDigit(current) && char.IsUpper(next))
+                {
+                    if (char.IsUpper(current))
+                    {
+                        if (!isLast && !isSecondFromLast && !char.IsUpper(afterNext))
+                            buffer += "_";
+                    }
+                    else
+                        buffer += "_";
+                }
+
+                if (!char.IsDigit(current) && char.IsDigit(next))
+                    buffer += "_";
+                if (char.IsDigit(current) && !char.IsDigit(next) && !isLast)
+                    buffer += "_";
+            }
+
+            return buffer;
+        }
+    }
+}
---
+++ JsonSerializer.SnakeCase/SnakeCaseJsonSerializer.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace JsonSerializer.SnakeCase
+{
+    public class SnakeCaseJsonSerializer : Newtonsoft.Json.JsonSerializer
+    {
+        public override IContractResolver ContractResolver
+        {
+            get { return new SnakeCaseContractResolver(); }
+        }
+        
+        public SnakeCaseJsonSerializer()
+        {
+            Converters.Add(new StringEnumConverter
+            {
+                AllowIntegerValues = true,
+                CamelCaseText = false
+            });
+        }
+    }
+}
