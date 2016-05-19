ba3655f5e51d490d2ba93c2bf4d9056ed9e61497
Steven Atkinson <steven.atkinson@gmx.com>
2016-05-19 11:08:44 +0100

    Initial commit.

---
+++ JsonSerializer.SnakeCase/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JsonSerializer.SnakeCase")]
+[assembly: AssemblyDescription("Json Serializer that converts property names to snake_case in the response")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("mrstebo")]
+[assembly: AssemblyProduct("JsonSerializer.SnakeCase")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8b2729de-29fd-4dfc-8da8-fd7da8f826d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
