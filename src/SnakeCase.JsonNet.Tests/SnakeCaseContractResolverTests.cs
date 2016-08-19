771a4e74855e686de66b71062e89178278d1ca84
Steven Atkinson <steven.atkinson@gmx.com>
2016-08-19 09:13:06 +0100

    Added additional tests to make sure that special cases are being handled.

--- src/SnakeCase.JsonNet.Tests/SnakeCaseContractResolverTests.cs
+++ src/SnakeCase.JsonNet.Tests/SnakeCaseContractResolverTests.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace SnakeCase.JsonNet.Tests
@@ -21,7 +22,7 @@ namespace SnakeCase.JsonNet.Tests
         }
 
         [Test]
-        public void SerializeObject_With_SnakeCaseContractResolver_Should_Convert_Property_Names_To_Snake_Case()
+        public void Should_Convert_Property_Names_To_Snake_Case()
         {
             var obj = new TestObject { Title = "Mr", FirstName = "John", LastName = "Smith" };
 
@@ -35,7 +36,63 @@ namespace SnakeCase.JsonNet.Tests
             Assert.That(result.Contains("last_name"));
         }
 
-        class TestObject
+        [Test]
+        public void Should_Handle_Special_Symbols()
+        {
+            var obj = new Dictionary<string, string>
+            {
+                {"test[user]", "Test"},
+                {"test-user", "Test"},
+                {"Test[Password]", "Test"},
+                {"Test-Password", "Test"}
+            };
+
+            var result = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+            {
+                ContractResolver = _contractResolver
+            });
+
+            Assert.That(result.Contains("test[user]"));
+            Assert.That(result.Contains("test_user"));
+        }
+
+        [Test]
+        public void Should_Handle_Consecutive_Uppercase_Characters()
+        {
+            var obj = new Dictionary<string, string>
+            {
+                {"TESTPrefix", "Test"},
+                {"TestSUFFIX", "Test"}
+            };
+
+            var result = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+            {
+                ContractResolver = _contractResolver
+            });
+
+            Assert.That(result.Contains("test_prefix"));
+            Assert.That(result.Contains("test_suffix"));
+        }
+
+        [Test]
+        public void Should_Handle_Digits()
+        {
+            var obj = new Dictionary<string, string>
+            {
+                {"Test123", "Test"},
+                {"123Test", "Test"}
+            };
+
+            var result = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+            {
+                ContractResolver = _contractResolver
+            });
+
+            Assert.That(result.Contains("test123"));
+            Assert.That(result.Contains("123_test"));
+        }
+
+        private class TestObject
         {
             public string Title { get; set; }
             public string FirstName { get; set; }
